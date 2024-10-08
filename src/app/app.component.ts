aa81a521e4dc90aa3c0aadff07a34aec6e1a9ac3
Viorel Strogoteanu <viorelss@icloud.com>
2024-10-08 14:20:04 +0200

    *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ng-basics';
   isVisible = true;
+  persone = [
+    { nome: 'luca', cognome: 'rossi', isOnline: true },
+    { nome: 'marco', cognome: 'verdi', isOnline: false },
+    { nome: 'anna', cognome: 'neri', isOnline: false },
+    { nome: 'leonardo', cognome: 'pannochia', isOnline: true },
+    { nome: 'flavio', cognome: 'babbo', isOnline: false },
+  ];
 
   onInput = (event: Event) => {
     this.title = (<HTMLInputElement>event.target).value;
